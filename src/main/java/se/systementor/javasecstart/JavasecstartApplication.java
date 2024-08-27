f1d191434e23c7004730dcb4cc2a933a3a3c9860
Mattias Seth <mattias.seth@gmail.com>
2024-08-27 09:54:38 +0200

    dataSeeder

--- src/main/java/se/systementor/javasecstart/JavasecstartApplication.java
+++ src/main/java/se/systementor/javasecstart/JavasecstartApplication.java
@@ -1,13 +1,24 @@
 package se.systementor.javasecstart;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import se.systementor.javasecstart.Security.UserDataSeeder;
 
 @SpringBootApplication
 public class JavasecstartApplication {
 
+    @Autowired
+    private UserDataSeeder userDataSeeder;
     public static void main(String[] args) {
         SpringApplication.run(JavasecstartApplication.class, args);
     }
-
+    @Bean
+    CommandLineRunner commandLineRunner() {
+        return args -> {
+            userDataSeeder.Seed();
+        };
+    }
 }
