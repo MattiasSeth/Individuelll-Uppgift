acfcd52e3694b79b58e3eed7c14cd54f559d7ccd
Mattias Seth <mattias.seth@gmail.com>
2024-09-02 18:13:11 +0200

    Hash to Password done.

--- src/main/java/se/systementor/javasecstart/Hash/HashController.java
+++ src/main/java/se/systementor/javasecstart/Hash/HashController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
@@ -20,11 +24,14 @@ public class HashController {
     public String showHash (){
         return "hash";
     }
-
+    @RequestMapping(path = "/reversehash")
+    public String showReverseHash () {
+        return "reversehash";
+    }
     @PostMapping(path = "/hashed")
     public String processHash(@RequestParam("txt") String text, Model model, HttpServletRequest request) {
-        System.out.println("Request Method: " + request.getMethod());
-        System.out.println("Request URI: " + request.getRequestURI());
+        //System.out.println("Request Method: " + request.getMethod());
+        //System.out.println("Request URI: " + request.getRequestURI());
 
         String sha256 = SHA256(text);
         String md5 = md5(text);
@@ -32,6 +39,13 @@ public class HashController {
         model.addAttribute("sha256", sha256);
         model.addAttribute("md5", md5);
         return "hashed";
+    }
+
+    @PostMapping(path = "reversehashed")
+    public String processHashToPassword (@RequestParam("txt") String text, Model model){
+        String password = password(text);
+        model.addAttribute("password", password);
+        return "reversehashed";
     }
 
     public String SHA256 (String string){
@@ -58,6 +72,27 @@ public class HashController {
             }
         System.out.println(hashtext);
         return hashtext;
+    }
+
+    public String password (String input){
+        String result = "Password not found";
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("hashes.txt"));
+            String text;
+
+            while ((text = reader.readLine()) != null) {
+                String[] parts = text.split(":");
+                if (parts[1].equals(input)) {
+                    result = parts[0];
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 
 }
